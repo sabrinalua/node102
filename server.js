3b80647a35c73839942c68b786a7b0313a92a0a6
Sabrina Lua <sabrinalua@MYPJMAD152MBP-SabrinaLua.local>
2017-07-12 10:43:30 +0800

    ready for heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-var express = require('express'), fs=require('fs'), app= express(), sitePath = process.argv[2] || "."; var port = 8080;
+var express = require('express'), bodyParser =require('body-parser'),fs=require('fs'), app= express(), sitePath = process.argv[2] || "."; var port = 8080;
 
+app.use(bodyParser.json());
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   fs.readFile('./view/index.html', function(err,data){
@@ -11,6 +12,11 @@ app.get('/snake', function (req, res) {
   fs.readFile('./Snake/index.html', function(err,data){
 	res.end(data);
 	});	
+})
+
+app.post('/post', function(req,res){
+	console.log(req.body);
+	res.send(req.body);
 })
 
 app.use(express.static(__dirname + '/' + sitePath));
