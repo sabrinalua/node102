2fd4f6939ba0c17b93e8501ed209610a543159cf
Sabrina Lua <sabrinalua@MYPJMAD152MBP-SabrinaLua.local>
2017-07-12 15:29:50 +0800

    add postgres

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express'), bodyParser =require('body-parser'),fs=require('fs'), app= express(), sitePath = process.argv[2] || "."; var port = 8000;
 
+var pg = require('pg');
+
 app.use(bodyParser.json());
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
@@ -22,6 +24,18 @@ app.post('/post', function(req,res){
 	console.log(req.body);
 	res.send(req.body);
 })
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 app.use(express.static(__dirname + '/' + sitePath));
 
