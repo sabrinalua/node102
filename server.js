1c5bbe9d7a881690dcdf17cf89cc14a28c658e79
Sabrina Lua <sabrinalua@MYPJMAD152MBP-SabrinaLua.local>
2017-07-12 10:47:55 +0800

    x

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express'), bodyParser =require('body-parser'),fs=require('fs'), app= express(), sitePath = process.argv[2] || "."; var port = 8080;
+var express = require('express'), bodyParser =require('body-parser'),fs=require('fs'), app= express(), sitePath = process.argv[2] || "."; var port = 8000;
 
 app.use(bodyParser.json());
 // respond with "hello world" when a GET request is made to the homepage
@@ -14,6 +14,10 @@ app.get('/snake', function (req, res) {
 	});	
 })
 
+/*
+curl -d '{"MyKey":"My Value"}' -H "Content-Type: application/json" http://localhost:8080/post
+{"MyKey":"My Value"}
+*/
 app.post('/post', function(req,res){
 	console.log(req.body);
 	res.send(req.body);
@@ -23,3 +27,4 @@ app.use(express.static(__dirname + '/' + sitePath));
 app.listen(port, function() { 
     console.log("Server running at: http://localhost:" + port)
 });
+
